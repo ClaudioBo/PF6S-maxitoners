a872d54ef4eb17c1ba84e9e265e368a5637cce23
Alfonso <alfonso_sanches@live.com.mx>
2021-03-15 15:54:39 -0700

    clase 'Alfonso' terminada

---
+++ src/mx/maxitoners/Alfonso.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.maxitoners;
+
+/**
+ *
+ * @author Alfonso
+ */
+public class Alfonso {
+    private int tacos;
+
+    /**
+     * @return the tacos
+     */
+    public int getTacos() {
+        return tacos;
+    }
+
+    /**
+     * @param tacos the tacos to set
+     */
+    public void setTacos(int tacos) {
+        this.tacos = tacos;
+    }
+    
+    
+}
